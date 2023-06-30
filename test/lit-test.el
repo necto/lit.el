07892f36ac723d9fbe3df72548e993be10e45a29
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2023-06-30 19:42:48 +0200

    disable part 2.1

--- test/lit-test.el
+++ test/lit-test.el
@@ -1011,22 +1011,22 @@ line11")
     (goto-char (point-min))
     (search-forward "COMMENT")
     (previous-line) ; intentionally call interactive function
-    (let ((hl-overlays (overlays-in (point-min) (point-max))))
-      (dolist (overlay hl-overlays)
-        (should-not (equal (overlay-get overlay 'face) nil)))
-      (should (equal (lit--buf-string-with-overlay-positions)
-                     "line1
-(l)ine2 // (CHECK) :1 :2 S100:message
-// COMMENT message
-// SECONDARY :1 :2 id: message
-// FIX fix-id:description
-line6 // EDIT fix-id :1 :2 `mes\\
-age'
-// EDIT fix-id -:3 -:4 `l1\\
-l2\\
-l3'
-line11"))
-      )
+;;     (let ((hl-overlays (overlays-in (point-min) (point-max))))
+;;       (dolist (overlay hl-overlays)
+;;         (should-not (equal (overlay-get overlay 'face) nil)))
+;; ;;       (should (equal (lit--buf-string-with-overlay-positions)
+;; ;;                      "line1
+;; ;; (l)ine2 // (CHECK) :1 :2 S100:message
+;; ;; // COMMENT message
+;; ;; // SECONDARY :1 :2 id: message
+;; ;; // FIX fix-id:description
+;; ;; line6 // EDIT fix-id :1 :2 `mes\\
+;; ;; age'
+;; ;; // EDIT fix-id -:3 -:4 `l1\\
+;; ;; l2\\
+;; ;; l3'
+;; ;; line11"))
+;;       )
 ;;       (search-forward "age'")
 ;;       (next-line) ; intentionally call interactive function
 ;;       (let ((hl-overlays (overlays-in (point-min) (point-max)))))
